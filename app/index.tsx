11566813eb26beec0ee31587f1d4e0393e8309b2
steven.chang <steven.chang@ringus-solution.com>
2025-01-14 14:19:03 +0800

    feat: implement textInput with scrollView

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,50 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, ScrollView } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Muffin" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState(initialList);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    if (!value) return;
+    setShoppingList([
+      ...shoppingList,
+      { id: String(shoppingList.length + 1), name: value },
+    ]);
+    setValue("");
+  };
+
   return (
-    <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Muffin" />
-    </View>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}
+    >
+      <TextInput
+        placeholder="E.g. Coffee"
+        style={styles.textInput}
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem key={item.id} name={item.name} />
+      ))}
+    </ScrollView>
   );
 }
 
@@ -16,6 +52,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    padding: 12,
+  },
+  contentContainer: {
+    paddingBottom: 24,
+  },
+  textInput: {
+    padding: 12,
+    margin: 12,
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: theme.colorLightGrey,
+    fontSize: 18,
+    backgroundColor: theme.colorWhite,
   },
 });
