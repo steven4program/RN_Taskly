7eb289cfb194f509faba1b9dcef079e9328b83d2
steven.chang <steven.chang@ringus-solution.com>
2025-02-03 17:54:19 +0800

    feat: implement history screen to display completed countdowns

--- app/counter/history.tsx
+++ app/counter/history.tsx
@@ -1,20 +1,66 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { countdownStorageKey, PersistedCountdownState } from ".";
+import { getFromStorage } from "../../utils/storage";
+import { format } from "date-fns";
+import { theme } from "../../theme";
+
+const fullDateFormate = "LLL d yyyy, h:mm aaa";
 
 export default function HistoryScreen() {
+  const [countdownState, setCountdownState] =
+    useState<PersistedCountdownState>();
+
+  useEffect(() => {
+    const init = async () => {
+      const value = await getFromStorage(countdownStorageKey);
+      setCountdownState(value);
+    };
+    init();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>History</Text>
-    </View>
+    <FlatList
+      style={styles.list}
+      contentContainerStyle={styles.contentContainer}
+      ListEmptyComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text style={styles.listItemText}>No history</Text>
+        </View>
+      }
+      data={countdownState?.completedAtTimestamp}
+      renderItem={({ item }) => (
+        <View style={styles.listItem}>
+          <Text style={styles.listItemText}>
+            {format(item, fullDateFormate)}
+          </Text>
+        </View>
+      )}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  contentContainer: {
+    marginTop: 8,
+  },
+  listEmptyContainer: {
     justifyContent: "center",
     alignItems: "center",
+    marginVertical: 18,
   },
-  text: {
-    fontSize: 24,
+  list: {
+    backgroundColor: theme.colorWhite,
+    flex: 1,
+  },
+  listItem: {
+    backgroundColor: theme.colorLightGrey,
+    marginHorizontal: 8,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 8,
+  },
+  listItemText: {
+    fontSize: 18,
   },
 });
